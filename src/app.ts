1c19920090b8b2e8defc879dc3702a03d959f1f5
Jose Bieco <jo_bieco@hotmail.com>
2021-08-30 23:39:49 -0300

    feat(app.ts): injected TYPE connection

--- src/app.ts
+++ src/app.ts
@@ -3,17 +3,19 @@ import { inject, injectable } from 'inversify'
 import { IFastify, TYPE } from './types/types'
 
 import { Routes } from './routes'
+import * as mongoose from 'mongoose'
 
-import 'reflect-metadata'
-
+type MongoProvider = () => Promise<typeof mongoose>
 @injectable()
 export class App {
   public constructor(
     @inject(TYPE.routes) _routes: Routes,
     @inject(TYPE.fastify) private readonly fastify: IFastify,
+    @inject(TYPE.connection) private readonly mongoProvider: MongoProvider,
   ) {}
 
   public listen(port: number | string): void {
     this.fastify.listen(port)
+    this.mongoProvider().then((response) => { response.model})
   }
 }
