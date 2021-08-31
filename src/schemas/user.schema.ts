69ea7538b9b35b41852126dd0220b2b4fa88143c
Jose Bieco <jo_bieco@hotmail.com>
2021-08-30 23:31:03 -0300

    feat(schemas/user): created the schema for user model

---
+++ src/schemas/user.schema.ts
@@ -0,0 +1,11 @@
+import { Schema, model } from 'mongoose';
+
+const userSchema = new Schema({
+  name:     { type: String, required: true },
+  score:    { type: Number, required: false },
+  email:    { type: String, required: true },
+  password: { type: String, required: true },
+  roles:    { type: [String], required: true }
+});
+
+export const User = model('User', userSchema);
