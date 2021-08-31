a8c02f0f0a8b7c01b9a1630987e67c4b1c9184c3
Jose Bieco <jo_bieco@hotmail.com>
2021-08-30 23:41:57 -0300

    feat(config/config.ts): binded TYPE connection, create connection

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,5 @@
 import fastify from 'fastify'
-import { Container } from 'inversify'
+import { Container, interfaces } from 'inversify'
 
 import { TYPE } from '../types/types'
 
@@ -9,12 +9,24 @@ import { UserController } from '../controllers/user.controller'
 
 import { App } from '../app'
 import { Routes } from '../routes'
+import * as mongoose from 'mongoose'
 
 export const container = new Container()
 
 container.bind(TYPE.app).to(App).inSingletonScope()
 container.bind(TYPE.routes).to(Routes).inSingletonScope()
 container.bind(TYPE.fastify).toConstantValue(fastify())
+container.bind(TYPE.connection).toProvider<typeof mongoose>(() => {
+  return (async () => {
+    try {
+      return await mongoose.connect('mongodb+srv://admin:admin@cluster0.qgfmv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+    }catch(err) {
+      console.error(err);
+    }
+  });
+})
+
+// const provider = container.get<Promise<typeof mongoose>>()
 
 setupUser()
 
