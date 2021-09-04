11ba5042c8dcd1681a5b56971567db8f14e859dc
Jose Bieco <jo_bieco@hotmail.com>
2021-09-03 22:30:45 -0300

    feat(main): reflect-metadata

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
+import 'reflect-metadata'
+
 import { TYPE } from './types/types'
 
 import { App } from './app'
 import { container } from './config/config'
 import { config } from 'dotenv'
 
+
 config()
 
-function bootstrap(): void {
+function bootstrap():void {
   const app = container.get<App>(TYPE.app)
   app.listen(process.env.PORT || 3000)
 }
