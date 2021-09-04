11f09e81d60eca859389a7e9dad97043cffa0dbe
Jose Bieco <jo_bieco@hotmail.com>
2021-09-03 22:39:58 -0300

    feat(user.service): create function and set id on getOne

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,10 +1,16 @@
 import { injectable } from 'inversify'
 
-import 'reflect-metadata'
+import { User } from '../schemas/user.schema';
 
 @injectable()
 export class UserService {
-  public getOne(): string {
-    return 'Hello World!'
+  public getOne(id:string) {
+    return User.findById(id);
+  }
+
+  public create(payload: any): any{
+    const user = new User(payload);
+    user.save();
+    return '200 - OK';
   }
 }
