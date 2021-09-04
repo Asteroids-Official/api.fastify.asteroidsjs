875efae76a752388774f53b96c5b9998b81d3071
Jose Bieco <jo_bieco@hotmail.com>
2021-09-03 22:29:59 -0300

    feat(app): mongo provider init

--- src/app.ts
+++ src/app.ts
@@ -8,14 +8,21 @@ import * as mongoose from 'mongoose'
 type MongoProvider = () => Promise<typeof mongoose>
 @injectable()
 export class App {
+  private db: typeof mongoose;
+
   public constructor(
     @inject(TYPE.routes) _routes: Routes,
     @inject(TYPE.fastify) private readonly fastify: IFastify,
     @inject(TYPE.connection) private readonly mongoProvider: MongoProvider,
-  ) {}
+  ) {
+    this.init();
+  }
 
   public listen(port: number | string): void {
     this.fastify.listen(port)
-    this.mongoProvider().then((response) => { response.model})
+  }
+
+  private async init(): Promise<void> {
+    this.db = await this.mongoProvider();
   }
 }
