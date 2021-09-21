cbd9477fcd875056a8070bee48a234e39c3d60a8
Jose Bieco <jo_bieco@hotmail.com>
2021-09-21 09:39:50 -0300

    feat(routes): created delete user route

--- src/routes.ts
+++ src/routes.ts
@@ -17,27 +17,41 @@ export class Routes {
   }
 
   private setupUserRoutes(): void {
-    this.fastify.get('/users', async (request) => {
-      return this.userController.getALl();
+    this.fastify.get('/users', async (request, response) => {
+      try{
+        const users = await this.userController.getALl();
+        response.send(users);
+      }catch(error){
+        response.status(error.statusCode).send(error);
+     }
     });
 
-    this.fastify.get('/users/:id', async (request) => {
-      return this.userController.getOne(getIdFromRequest(request));
-    });
-
-    this.fastify.post('/users', async(request) => {
-      return this.userController.create(request.body);
+    this.fastify.get('/users/:id', async (request, response) => {
+      try{
+        const user = await this.userController.getOne(getIdFromRequest(request));
+        response.send(user);
+      }catch(error){
+        response.status(error.statusCode).send(error);
+      }
     });
 
     this.fastify.post('/register', async(request, response) => {
      //TODO: hash and salt
 
      try{
-        //response.code(201);
         response.send(this.userController.create(request.body));
      }catch(error){
-        response.send(error);
+      response.status(error.statusCode).send(error);
      }
+    });
+
+    this.fastify.delete('/users/:id', async (request, response) => {
+      try{
+        await this.userController.delete(getIdFromRequest(request))
+        response.status(200).send();
+      }catch(error){
+        response.status(error.statusCode).send(error);
+      }
     });
   }
 }
