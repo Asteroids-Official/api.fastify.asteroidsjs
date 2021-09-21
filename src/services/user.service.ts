59010678c52b1347cc3da61fcd6a4a00b7bd053e
Jose Bieco <jo_bieco@hotmail.com>
2021-09-21 09:42:57 -0300

    feat(services/user.service): created delete function

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -5,7 +5,7 @@ import { User } from '../schemas/user.schema';
 @injectable()
 export class UserService {
 
-  public getAll() {
+  public async getAll() {
     return User.find();
   }
 
@@ -18,4 +18,9 @@ export class UserService {
     user.save();
     return user;
   }
+
+  public delete(id:string) {
+    return User.findByIdAndDelete(id);
+  }
+
 }
