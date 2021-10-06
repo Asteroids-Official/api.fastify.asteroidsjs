427d9908c09fd3768cf71db0304562331d4edc65
Jose Bieco <jo_bieco@hotmail.com>
2021-10-05 22:21:34 -0300

    docs(src/entities/spaceship/controllers/spaceship.controller): routes

---
+++ src/entities/spaceship/controllers/spaceship.controller.ts
@@ -0,0 +1,10 @@
+/**
+- createOne       (POST /spaceships)
+- getDefault      (GET /spaceship/default)
+- getOne          (GET /spaceships/:id)
+- getMany         (GET /spaceships)
+- updateOne       (PATCH /spaceships/:id)
+- deleteOne       (DELETE /spaceships/:id)
+- disableOne      (PUT /spaceships:/id)
+- enableOne       (PUT /spaceships:/id)
+ */
