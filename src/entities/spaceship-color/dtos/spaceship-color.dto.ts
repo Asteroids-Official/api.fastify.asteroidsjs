0083fd93d1b4b064ba0493e96cfea1e1112f09b4
Jose Bieco <jo_bieco@hotmail.com>
2021-10-05 22:23:44 -0300

    feat(src/entities/spaceship-color/dtos/spaceship-color.dto): removed id

--- src/entities/spaceship-color/dtos/spaceship-color.dto.ts
+++ src/entities/spaceship-color/dtos/spaceship-color.dto.ts
@@ -2,10 +2,6 @@ import { BaseDto } from '../../../shared/base.dto'
 
 import { IsDefined, IsString, IsOptional, IsBoolean } from 'class-validator'
 export class SpaceshipColorDto extends BaseDto {
-  @IsDefined({ message: 'It is required to send the id' })
-  @IsString({ message: 'It is required to send a valid string' })
-  _id: string
-
   @IsDefined({ message: 'It is required to send the url' })
   @IsString({ message: 'It is required to send a valid string' })
   url: string
