79b51678f0bb65dbb411a3e713d5dabf6cf308e0
Jose Bieco <jo_bieco@hotmail.com>
2021-10-06 21:35:05 -0300

    feat(src/entities/spaceship/dtos/spaceship.dto): dto for spaceship with class validator

--- src/entities/spaceship/dtos/spaceship.dto.ts
+++ src/entities/spaceship/dtos/spaceship.dto.ts
@@ -2,7 +2,7 @@ import { BaseDto } from "../../../shared/base.dto";
 import { ColliderDto } from "../../collider/dtos/collider.dto";
 import { SpaceshipColorDto } from "../../spaceship-color/dtos/spaceship-color.dto";
 
-import { IsDefined, IsArray, IsString, IsOptional } from 'class-validator'
+import { IsDefined, IsArray, IsString, IsOptional, IsBoolean } from 'class-validator'
 
 export class SpaceshipDto extends BaseDto {
   @IsDefined({ message: 'It is required to send the name' })
@@ -16,4 +16,12 @@ export class SpaceshipDto extends BaseDto {
   @IsOptional({ message: 'It is required to send the spaceshipColors' })
   @IsArray({ message: 'It is required to send a valid array' })
   spaceshipColors?: SpaceshipColorDto[]
+
+  @IsOptional({ message: 'It is required to send if is active' })
+  @IsBoolean({ message: 'It is required to send a valid boolean' })
+  isActive?: boolean
+
+  @IsDefined({ message: 'It is required to send if is default' })
+  @IsBoolean({ message: 'It is required to send a valid boolean' })
+  isDefault: boolean
 }
